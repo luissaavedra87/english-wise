59372883d1b5bbe74dd256f5ffe98fa959f1e060
Luis Saavedra <nriqu322@gmail.com>
2020-10-07 16:50:56 -0600

    fix lint errors

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,8 @@ const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path='/login' component={Login} />
-      <Route path='/signup' component={Signup} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
     </Switch>
   </Router>
 );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
