cb4ef8f99baa89b4edd9c3b8941d00f3e664f9bd
Luis Saavedra <nriqu322@gmail.com>
2020-10-09 13:04:09 -0600

    add signup page and singup post to api

--- src/services/englishWiseApi.js
+++ src/services/englishWiseApi.js
@@ -13,7 +13,7 @@ const englishWiseApi = () => {
     try {
       const response = await fetch(`${baseUrl}/users/login`, config);
       const data = await response.json();
-      return data.results;
+      return data;
     } catch (e) {
       return {
         error: e.message,
@@ -21,7 +21,26 @@ const englishWiseApi = () => {
     }
   };
 
-  return userLogin;
+  const userSignup = async user => {
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    };
+    try {
+      const response = await fetch(`${baseUrl}/users/signup`, config);
+      const data = await response.json();
+      return data;
+    } catch (e) {
+      return {
+        error: e.message,
+      };
+    }
+  };
+
+  return { userLogin, userSignup };
 };
 
 export default englishWiseApi;
