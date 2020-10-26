d9e0c6efd00e2e83235e6087785033c3c05d5095
Luis Saavedra <nriqu322@gmail.com>
2020-10-26 17:45:47 -0600

    add teachers list carousel

--- src/containers/TeachersList.js
+++ src/containers/TeachersList.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import { getTeachers } from '../services/englishWiseApi';
 
-const TeachersList = () => {
+class TeachersList extends React.Component {
+  responsive = {
+    0: { items: 1 },
+  }
 
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      teachers: [],
+    };
+  }
+
+  componentDidMount() {
+    getTeachers()
+      .then(response => {
+        if (response) {
+          console.log(response[0].image);
+          const image = response.map(m => <img key={m.id} src={m.image} alt="teachers" />);
+          this.setState({
+            teachers: image,
+          });
+        }
+      });
+  }
+
+  render() {
+    const { teachers } = this.state;
+
+    return (
+      <div>
+        <div>Pick a Teacher</div>
+        <div>
+          <AliceCarousel
+            items={teachers}
+            responsive={this.responsive}
+            autoPlayDirection="rtl"
+            autoPlayInterval={2500}
+            autoPlay
+            fadeOutAnimation
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default TeachersList;
