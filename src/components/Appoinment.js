fca1a873650ca0de722d84343dc3413367399f67
Luis Saavedra <nriqu322@gmail.com>
2020-10-28 12:30:34 -0600

    add appoinment input to teachers details

---
+++ src/components/Appoinment.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+
+// const Appoinment = () => {
+//   return (
+
+//   )
+// };
+
+// export default Appoinment;
--- src/components/teacherDetails.js
+++ src/components/teacherDetails.js
@@ -10,18 +10,27 @@ const TeacherDetails = props => {
   const currentTeacher = teachers[id];
 
   return (
-    <div className="teacher-details">
-      <div>
-        <div className="container-back">
-          <Link to="/teachers">
-            <h2 className="back">Go Back</h2>
-          </Link>
-        </div>
+    <div className="teacher-details-container">
+      <div className="container-back">
+        <Link to="/teachers">
+          <h2 className="back">Go Back</h2>
+        </Link>
+      </div>
+      <div className="teacher-details">
         <img className="details-teacher-img" src={currentTeacher.image} alt="teacher" />
         <h4>Name</h4>
         <p>{currentTeacher.name}</p>
         <h4>Description</h4>
         <p>{currentTeacher.description}</p>
+        <form>
+          <h4>Schedule a Class:</h4>
+          <label htmlFor="teacher-schedule">
+            Select your preferred day and time between 8:00am and 4:00pm
+            <input id="teacher-schedule" type="datetime-local" step="60000" required />
+          </label>
+          <input type="submit" value="Book Schedule" />
+        </form>
+
       </div>
     </div>
   );
