8027a412616a2ba93cb8705975f619b962fb3c98
Luis Saavedra <nriqu322@gmail.com>
2020-11-10 18:30:16 -0600

    redirect after reload details page

--- src/components/TeacherDetails.js
+++ src/components/TeacherDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import { Link, Redirect, withRouter } from 'react-router-dom';
 import AppoinmentForm from './AppoinmentForm';
 
 const TeacherDetails = props => {
@@ -10,56 +10,34 @@ const TeacherDetails = props => {
   const { teachers } = props;
   const currentTeacher = teachers[id];
 
-  return (
-    // { currentTeacher ? (
-    //   <>
-    //     <div className="teacher-details-container">
-    //       <div className="container-back">
-    //         <Link to="/teachers">
-    //           <h4 className="back ml-5">Go Back</h4>
-    //         </Link>
-    //       </div>
-    //       <div className="teacher-details">
-    //         <div className="d-flex justify-content-around mb-3">
-    //           <img className="details-teacher-img mx-5" src={currentTeacher.image} alt="teacher" />
-    //           <div className="d-flex flex-column mr-5 justify-content-center">
-    //             <h4>Name</h4>
-    //             <p>{currentTeacher.name}</p>
-    //             <h4>Teacher Id</h4>
-    //             <p>{currentTeacher.id}</p>
-    //             <h4>Description</h4>
-    //             <p>{currentTeacher.description}</p>
-    //           </div>
-    //         </div>
-    //         <hr />
-    //         <AppoinmentForm currentTeacherId={currentTeacher.id} />
-    //       </div>
-    //     </div>
-    //   </>
-    // ) : <Redirect to="/login" />}
-    <div className="teacher-details-container">
-      <div className="container-back">
-        <Link to="/teachers">
-          <h4 className="back ml-5">Go Back</h4>
-        </Link>
-      </div>
-      <div className="teacher-details">
-        <div className="d-flex justify-content-around mb-3">
-          <img className="details-teacher-img mx-5" src={currentTeacher.image} alt="teacher" />
-          <div className="d-flex flex-column mr-5 justify-content-center">
-            <h4>Name</h4>
-            <p>{currentTeacher.name}</p>
-            <h4>Teacher Id</h4>
-            <p>{currentTeacher.id}</p>
-            <h4>Description</h4>
-            <p>{currentTeacher.description}</p>
-          </div>
+  if (currentTeacher) {
+    return (
+      <div className="teacher-details-container">
+        <div className="container-back">
+          <Link to="/teachers">
+            <h4 className="back ml-5">Go Back</h4>
+          </Link>
         </div>
-        <hr />
-        <AppoinmentForm currentTeacherId={currentTeacher.id} />
+        <div className="teacher-details">
+          <div className="d-flex justify-content-around mb-3">
+            <img className="details-teacher-img mx-5" src={currentTeacher.image} alt="teacher" />
+            <div className="d-flex flex-column mr-5 justify-content-center">
+              <h4>Name</h4>
+              <p>{currentTeacher.name}</p>
+              <h4>Teacher Id</h4>
+              <p>{currentTeacher.id}</p>
+              <h4>Description</h4>
+              <p>{currentTeacher.description}</p>
+            </div>
+          </div>
+          <hr />
+          <AppoinmentForm currentTeacherId={currentTeacher.id} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <Redirect to="/" />;
 };
 
 TeacherDetails.propTypes = {
